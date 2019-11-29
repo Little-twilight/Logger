8bbfdbe7d8cbe31b28963962110962b7d7795185
macfred <macfredsde@outlook.com>
2019-11-29 14:04:32 +0800

    Add ZyLogger Wrapper

--- logger-core/src/main/java/com/github/naturs/logger/Logger.java
+++ logger-core/src/main/java/com/github/naturs/logger/Logger.java
@@ -10,140 +10,214 @@ import org.jetbrains.annotations.Nullable;
 /**
  * But more pretty, simple and powerful
  */
-public final class Logger {
+public class Logger {
 
-    public static final int VERBOSE = 2;
-    public static final int DEBUG = 3;
-    public static final int INFO = 4;
-    public static final int WARN = 5;
-    public static final int ERROR = 6;
-    public static final int ASSERT = 7;
+	public static final int VERBOSE = 2;
+	public static final int DEBUG = 3;
+	public static final int INFO = 4;
+	public static final int WARN = 5;
+	public static final int ERROR = 6;
+	public static final int ASSERT = 7;
 
-    private static Printer printer = new LoggerPrinter();
+	protected static Printer printer = new LoggerPrinter();
 
-    private Logger() {
-        throw new AssertionError("No instances.");
-    }
+	protected Logger() {
+		throw new AssertionError("No instances.");
+	}
 
-    public static void printer(Printer printer) {
-        if (printer != null) {
-            Logger.printer = printer;
-        }
-    }
+	public static void printer(Printer printer) {
+		if (printer != null) {
+			Logger.printer = printer;
+		}
+	}
 
-    public static void setDebuggable(boolean debuggable) {
-        Logger.printer.setDebuggable(debuggable);
-    }
+	public static void setDebuggable(boolean debuggable) {
+		Logger.printer.setDebuggable(debuggable);
+	}
 
-    public static void addLogAdapter(LogAdapter adapter) {
-        printer.addAdapter(adapter);
-    }
+	public static void addLogAdapter(LogAdapter adapter) {
+		printer.addAdapter(adapter);
+	}
 
-    public static void clearLogAdapters() {
-        printer.clearLogAdapters();
-    }
+	public static void clearLogAdapters() {
+		printer.clearLogAdapters();
+	}
 
-    public static void setLogConverter(LogConverter converter) {
-        if (converter != null) {
-            ObjectConverter.converter(converter);
-        }
-    }
+	public static void setLogConverter(LogConverter converter) {
+		if (converter != null) {
+			ObjectConverter.converter(converter);
+		}
+	}
 
-    public static void addConverterStrategy(ConverterStrategy strategy) {
-        ObjectConverter.add(strategy);
-    }
+	public static void addConverterStrategy(ConverterStrategy strategy) {
+		ObjectConverter.add(strategy);
+	}
 
-    @Deprecated
-    public static Printer t(String tag) {
-        return printer.t(tag);
-    }
+	@Deprecated
+	public static Printer t(String tag) {
+		return printer.t(tag);
+	}
 
-    /**
-     * Given tag will be used as tag only once for this method call regardless of the tag that's been
-     * set during initialization. After this invocation, the general tag that's been set will
-     * be used for the subsequent log calls
-     */
-    public static Printer tag(String tag) {
-        return printer.tag(tag);
-    }
+	/**
+	 * Given tag will be used as tag only once for this method call regardless of the tag that's been
+	 * set during initialization. After this invocation, the general tag that's been set will
+	 * be used for the subsequent log calls
+	 */
+	public static Printer tag(String tag) {
+		return printer.tag(tag);
+	}
 
-    /**
-     * Given FormatStrategy will be used only once for this method call regardless of the tag that's been
-     * set during initialization. After this invocation, the general strategy that's been set will
-     * be used for the subsequent log calls.
-     */
-    public static Printer strategy(FormatStrategy strategy) {
-        return printer.strategy(strategy);
-    }
+	/**
+	 * Given FormatStrategy will be used only once for this method call regardless of the tag that's been
+	 * set during initialization. After this invocation, the general strategy that's been set will
+	 * be used for the subsequent log calls.
+	 */
+	public static Printer strategy(FormatStrategy strategy) {
+		return printer.strategy(strategy);
+	}
 
-    public static Printer invokeClass(Class clazz) {
-        return printer.invokeClass(clazz);
-    }
+	public static Printer invokeClass(Class clazz) {
+		return printer.invokeClass(clazz);
+	}
 
-    /**
-     * General log function that accepts all configurations as parameter
-     */
-    public static void log(int priority, String tag, String message, Throwable throwable,
+	/**
+	 * General log function that accepts all configurations as parameter
+	 */
+	public static void log(int priority, String tag, String message, Throwable throwable,
                            @Nullable FormatStrategy strategy, @Nullable Class invokeClass) {
-        printer.log(priority, tag, message, throwable, strategy, invokeClass);
-    }
+		printer.log(priority, tag, message, throwable, strategy, invokeClass);
+	}
 
-    public static void d(String message, Object... args) {
-        printer.d(message, args);
-    }
+	public static void d(String message, Object... args) {
+		printer.d(message, args);
+	}
 
-    public static void obj(Object object) {
-        printer.obj(object);
-    }
+	public static void obj(Object object) {
+		printer.obj(object);
+	}
 
-    public static void obj(String message, Object object) {
-        printer.obj(message, object);
-    }
+	public static void obj(String message, Object object) {
+		printer.obj(message, object);
+	}
 
-    public static void obj(int priority, String message, Object object) {
-        printer.obj(priority, message, object);
-    }
+	public static void obj(int priority, String message, Object object) {
+		printer.obj(priority, message, object);
+	}
 
-    public static void e(String message, Object... args) {
-        printer.e(null, message, args);
-    }
+	public static void e(String message, Object... args) {
+		printer.e(null, message, args);
+	}
 
-    public static void e(Throwable throwable, String message, Object... args) {
-        printer.e(throwable, message, args);
-    }
+	public static void e(Throwable throwable, String message, Object... args) {
+		printer.e(throwable, message, args);
+	}
 
-    public static void i(String message, Object... args) {
-        printer.i(message, args);
-    }
+	public static void i(String message, Object... args) {
+		printer.i(message, args);
+	}
 
-    public static void v(String message, Object... args) {
-        printer.v(message, args);
-    }
+	public static void v(String message, Object... args) {
+		printer.v(message, args);
+	}
 
-    public static void w(String message, Object... args) {
-        printer.w(message, args);
-    }
+	public static void w(String message, Object... args) {
+		printer.w(message, args);
+	}
 
-    /**
-     * Tip: Use this for exceptional situations to log
-     * ie: Unexpected errors etc
-     */
-    public static void wtf(String message, Object... args) {
-        printer.wtf(message, args);
-    }
+	/**
+	 * Tip: Use this for exceptional situations to log
+	 * ie: Unexpected errors etc
+	 */
+	public static void wtf(String message, Object... args) {
+		printer.wtf(message, args);
+	}
 
-    /**
-     * Formats the given json content and print it
-     */
-    public static void json(String json) {
-        printer.json(json);
-    }
+	/**
+	 * Formats the given json content and print it
+	 */
+	public static void json(String json) {
+		printer.json(json);
+	}
 
-    /**
-     * Formats the given xml content and print it
-     */
-    public static void xml(String xml) {
-        printer.xml(xml);
-    }
+	/**
+	 * Formats the given xml content and print it
+	 */
+	public static void xml(String xml) {
+		printer.xml(xml);
+	}
+
+	public static void printException(String tag, Throwable throwable) {
+		printer.tag(tag);
+		printer.e(throwable, "");
+	}
+
+	public static void printException(Throwable throwable) {
+		printer.e(throwable, "");
+	}
+
+	public static void d(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.d(message, args);
+	}
+
+	public static void obj(String tag, String message, Object object) {
+		printer.tag(tag);
+		printer.obj(message, object);
+	}
+
+	public static void obj(int priority, String tag, String message, Object object) {
+		printer.tag(tag);
+		printer.obj(priority, message, object);
+	}
+
+	public static void e(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.e(message, args);
+	}
+
+	public static void e(String tag, Throwable throwable, String message, Object... args) {
+		printer.tag(tag);
+		printer.e(throwable, message, args);
+	}
+
+	public static void i(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.i(message, args);
+	}
+
+	public static void v(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.v(message, args);
+	}
+
+	public static void w(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.w(message, args);
+	}
+
+	/**
+	 * Tip: Use this for exceptional situations to log
+	 * ie: Unexpected errors etc
+	 */
+	public static void wtf(String tag, String message, Object... args) {
+		printer.tag(tag);
+		printer.wtf(message, args);
+	}
+
+	/**
+	 * Formats the given json content and print it
+	 */
+	public static void json(String tag, String json) {
+		printer.tag(tag);
+		printer.json(json);
+	}
+
+	/**
+	 * Formats the given xml content and print it
+	 */
+	public static void xml(String tag, String xml) {
+		printer.tag(tag);
+		printer.xml(xml);
+	}
 
 }
---
+++ logger-core/src/main/java/com/zhongyou/util/ZyLogger.java
@@ -0,0 +1,11 @@
+package com.zhongyou.util;
+
+import com.github.naturs.logger.Logger;
+
+public class ZyLogger extends Logger {
+
+	private ZyLogger() {
+		super();
+	}
+
+}
